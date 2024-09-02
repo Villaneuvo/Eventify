be62df9da4084095db5ecfd0d9c51b62b43fbb79
villaneuvo <111202012812@mhs.dinus.ac.id>
2024-09-02 16:41:15 +0700

    [build] add jumbotron (semi-responsive) fix navbar

--- apps/web/app/components/section/navigation/NavBar.tsx
+++ apps/web/app/components/section/navigation/NavBar.tsx
@@ -6,72 +6,106 @@ import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { NavigationButton } from "../../Button";
+import { NavigationButton } from "../../NavigationButton";
 import { SearchBar } from "../../SearchBar";
 
 const NavBar = () => {
-    const { status } = useSession();
-    const currentPath = usePathname();
+  const { status } = useSession();
+  const currentPath = usePathname();
 
-    let authPage = false;
-    if (currentPath === "/auth/login" || currentPath === "/auth/register") authPage = true;
+  let authPage = false;
+  if (currentPath === "/auth/login" || currentPath === "/auth/register")
+    authPage = true;
 
-    const links = [
-        { href: "/account", label: "Find events" },
-        { href: "/ticket", label: "Eventify your event" },
-        { href: "/transaction", label: "Jakarta Pusat" },
-    ];
+  const links = [
+    { href: "/account", label: "Find events" },
+    { href: "/ticket", label: "Eventify your event" },
+    { href: "/transaction", label: "Jakarta Pusat" },
+  ];
 
-    return (
-        <nav className="border-b">
-            <div className="flex h-20 items-center justify-between  px-5 xl:mx-[7.813rem]">
-                <div className="flex-shrink-0">
-                    <Link href="/">
-                        <Image src="/logo.svg" alt="Logo" width={100} height={100} className="mb-1" />
-                    </Link>
-                </div>
-                <div className="flex-grow flex justify-center">
-                    <SearchBar />
-                </div>
+  return (
+    <nav className="border-b">
+      <div className="flex h-20 items-center justify-between md:mx-[4.688rem] lg:mx-[6.25rem] xl:mx-[7.813rem]">
+        <div className="flex-shrink-0">
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={100}
+              height={100}
+              className="mb-1"
+            />
+          </Link>
+        </div>
+        <div className="flex flex-grow">
+          <SearchBar />
+        </div>
 
-                {!authPage && (
-                    <div className="flex-grow flex justify-end mr-6 translate-x-72 z-10">
-                        <ul className="flex space-x-6">
-                            {links.map((link) => (
-                                <Link
-                                    key={link.href}
-                                    href={link.href}
-                                    className={classNames({
-                                        "text-sm text-text-main font-bold": link.href === currentPath,
-                                        "text-sm text-text-main font-regular": link.href !== currentPath,
-                                        "transition-colors hover:text-main-color": true,
-                                    })}
-                                >
-                                    {link.label}
-                                </Link>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+        {!authPage && (
+          <div className="z-10 ml-10 md:-ml-4">
+            <ul className="flex">
+              {links.map((link) => (
+                <Link
+                  key={link.href}
+                  href={link.href}
+                  className={classNames({
+                    "rounded-[1.875rem] text-xs font-bold text-text-main lg:p-2 xl:p-3 xl:text-sm":
+                      link.href === currentPath,
+                    "font-regular rounded-[1.875rem] text-xs text-text-main lg:p-2 xl:p-3 xl:text-sm":
+                      link.href !== currentPath,
+                    "transition-colors hover:rounded-[1.875rem] hover:bg-bg-main hover:font-semibold hover:text-main-color":
+                      true,
+                    "lg:w-24 xl:w-28": link.label === "Find events",
+                    "lg:w-[138px] xl:w-40":
+                      link.label === "Eventify your event",
+                    "lg:w-28 xl:w-32": link.label === "Jakarta Pusat",
+                  })}
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </ul>
+          </div>
+          //   <div className="z-10 lg:translate-x-[8.5rem] xl:translate-x-[17.5rem]">
+          //   <ul className="flex">
+          //     {links.map((link) => (
+          //       <Link
+          //         key={link.href}
+          //         href={link.href}
+          //         className={classNames({
+          //           // Set individual widths for each link using Tailwind's `w-*` classes
+          //           "rounded-[1.875rem] p-3 text-sm font-bold text-text-main w-20": link.label === "Find events",
+          //           "rounded-[1.875rem] p-3 text-sm font-bold text-text-main w-32": link.label === "Eventify your event",
+          //           "rounded-[1.875rem] p-3 text-sm font-bold text-text-main w-16": link.label === "Jakarta Pusat",
+          //           "transition-colors hover:rounded-[1.875rem] hover:bg-bg-main hover:font-semibold hover:text-main-color":
+          //             true,
+          //         })}
+          //       >
+          //         {link.label}
+          //       </Link>
+          //     ))}
+          //   </ul>
+          // </div>
+        )}
 
-                {status === "authenticated" && (
-                    <div className="flex-shrink-0">
-                        <Button>
-                            <Link href="/api/auth/signout">Sign Out</Link>
-                        </Button>
-                    </div>
-                )}
-                {status === "unauthenticated" && (
-                    <div className="flex-shrink-0">
-                        {/* <Button>
+        {status === "authenticated" && (
+          <div className="flex-shrink-0">
+            <Button>
+              <Link href="/api/auth/signout">Sign Out</Link>
+            </Button>
+          </div>
+        )}
+        {status === "unauthenticated" && (
+          <div className="flex-shrink-0">
+            {/* <Button>
                         <Link href="/api/auth/signin">Sign In</Link>
                     </Button> */}
-                        <NavigationButton />
-                    </div>
-                )}
-            </div>
-        </nav>
-    );
+            <NavigationButton />
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,28 +1,43 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            },
-            colors: {
-                "bg-main": "#F5F5F5",
-                "main-color": "#4169E1",
-                "text-main": "#333333",
-            },
-            boxShadow: {
-                "nav-btn-shadow": "0 4px 12px 0 rgba(0, 0, 0, 0.25)",
-                "search-bar-shadow": "0 4px 4px 0 rgba(0, 0, 0, 0.15)",
-            },
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "magnifier-333": "url('/magnifier-333.png')",
+        "magnifier-555": "url('/magnifier-333.png')",
+      },
+      colors: {
+        "bg-main": "#F5F5F5",
+        "main-color": "#4169E1",
+        "text-main": "#333333",
+        "btn-jumbotron": "#1E3A8A",
+        "dark-charcoal": "#333333",
+      },
+      boxShadow: {
+        "nav-btn-shadow": "0 4px 12px 0 rgba(0, 0, 0, 0.25)",
+        "search-bar-shadow": "0 4px 4px 0 rgba(0, 0, 0, 0.15)",
+        "jumbtron-btn-shadow": "4px 4px 12px 0 rgba(65, 105, 225, 0.25)",
+      },
     },
-    plugins: [],
+    screens: {
+      xs: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+      "3xl": "1536px",
+    },
+  },
+  plugins: [],
 };
 export default config;
